7403366b4f20aba6308922d977b28644af954b57
Patrick Anderson <patrick20anderson@gmail.com>
2025-05-12 01:30:54 -0400

    Update transform_data.py

--- transform/transform_data.py
+++ transform/transform_data.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 def transform_data(df):
-    # Convert columns to numeric types
+    #Convert columns to numeric types
     df['view_count'] = pd.to_numeric(df['view_count'], errors='coerce')
     df['like_count'] = pd.to_numeric(df['like_count'], errors='coerce')
     df['comment_count'] = pd.to_numeric(df['comment_count'], errors='coerce')
 
-    # Create Engagement Rate (avoid division by zero)
+    #Create Engagement Rate (avoid division by zero)
     df['engagement_rate'] = (df['like_count'] + df['comment_count']) / df['view_count']
     df['engagement_rate'].fillna(0, inplace=True)
 
